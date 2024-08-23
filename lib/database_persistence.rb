f922001539e1875ba972d5dca04f90ffe0b19f2d
William <wguiraud@protonmail.com>
2024-08-23 12:12:28 -0700

    finishing the implementation of todo related methods

--- lib/database_persistence.rb
+++ lib/database_persistence.rb
@@ -81,25 +81,38 @@ class DatabasePersistence
   end
 
   def create_new_todo(list_id, todo_name)
-#    list = find_list(list_id)
-#    todo_id = next_element_id(list[:todos])
-#    list[:todos] << { id: todo_id, name: todo_name, completed: false }
+    sql = <<~SQL
+    INSERT INTO todos (name, list_id)
+    VALUES 
+    ($1, $2)
+    SQL
+    query(sql, todo_name, list_id)
   end
 
   def delete_todo_from_list(list_id, todo_id)
-#    list = find_list(list_id)
-#    list[:todos] .reject! { |todo| todo[:id] == todo_id }
+    sql = <<~SQL
+    DELETE FROM todos
+    WHERE id = $1 AND list_id = $2
+    SQL
+    query(sql, todo_id, list_id)
   end
 
   def update_todo_status(list_id, todo_id, new_status)
-#    list = find_list(list_id)
-#    todo = list[:todos].find { |t| t[:id] == todo_id }
-#    todo[:completed] = new_status
+    sql = <<~SQL
+    UPDATE todos
+    SET completed = $1
+    WHERE id = $2 AND list_id = $3
+    SQL
+    query(sql, new_status, todo_id, list_id)
   end
 
   def mark_all_todos_as_completed(list_id)
-#    list = find_list(list_id)
-#    list[:todos].each { |todo| todo[:completed] = true }
+    sql = <<~SQL
+    UPDATE todos
+    SET completed = true
+    WHERE list_id = $1
+    SQL
+    query(sql, list_id) 
   end
 
   private
